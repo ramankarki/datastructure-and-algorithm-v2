ee67328c0b2a458b54049d054b09746b3fe8ae9e
Raman Karki <ramankarki40@gmail.com>
2022-07-15 21:57:15 +0545

    quick sort

---
+++ algorithms/sorting/quick.js
@@ -0,0 +1,16 @@
+function quick(array) {
+  if (array.length <= 1) return array;
+
+  let selected = array[0];
+  let leftArr = [];
+  let rightArr = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < selected) leftArr.push(array[i]);
+    else rightArr.push(array[i]);
+  }
+
+  return [...quick(leftArr), select, ...quick(rightArr)];
+}
+
+console.log(quick([5, 6, 7, 8, 5, 4, 3, 2, 1, 4, 5, 6, 7, 8, 0]));
