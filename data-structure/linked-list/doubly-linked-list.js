98e5dfa50efc71dfcefa152273ba3281b7a54d44
Raman Karki <ramankarki40@gmail.com>
2022-07-17 13:26:04 +0545

    doubly linked list with push, pop, log, logReverse methods

---
+++ data-structure/linked-list/doubly-linked-list.js
@@ -0,0 +1,72 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      this.tail = node;
+      this.length++;
+      return node;
+    }
+
+    this.tail.next = node;
+    node.prev = this.tail;
+    this.tail = node;
+    this.length++;
+    return node;
+  }
+
+  pop() {
+    if (!this.length) return;
+
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    this.length--;
+  }
+
+  log() {
+    let activeNode = this.head;
+    let index = 0;
+    console.log('Length:', this.length);
+    while (activeNode) {
+      console.log({ index, value: activeNode.value });
+      activeNode = activeNode.next;
+      index++;
+    }
+  }
+
+  logReverse() {
+    let activeNode = this.tail;
+    let index = this.length - 1;
+    console.log('Length:', this.length);
+    while (activeNode) {
+      console.log({ index, value: activeNode.value });
+      activeNode = activeNode.prev;
+      index--;
+    }
+  }
+}
+
+const list = new LinkedList();
+
+list.push(5);
+list.push(4);
+list.push(3);
+
+list.pop();
+
+list.log();
+list.logReverse();
